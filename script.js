9f2e237dfed5310405faefb28452e7461fcbcaa4
Tmanhertzog <tmanhertzog@gmail.com>
2025-06-28 14:31:54 -0700

    added form

---
+++ script.js
@@ -0,0 +1,27 @@
+// Tristan Hertzog
+
+// let imageFile;
+// let imageAmount;
+// let horizontalFLip;
+// let verticalFlip;
+// let rotation;
+// let cropping;
+// let translation;
+// let shearing;
+// let brightness;
+// let contrast;
+// let saturation;
+// let invertColors;
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const form  = document.getElementById('myForm');
+//     console.log("test1")
+
+//     form.addEventListener('submit', e => {
+//         e.preventDefault();
+//         const email = document.getElementById('email').value;
+//         console.log(email);
+//   });
+// });
+
+document.querySelectorAll('#FileForm input')
